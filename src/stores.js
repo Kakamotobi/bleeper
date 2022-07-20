9ff0f90670a889201f4c13985855cf687d748201
Daeram Chung <daeram.chung@gmail.com>
2022-07-20 17:53:12 +0900

    Setup tasks store

--- src/stores.js
+++ src/stores.js
@@ -1,3 +1,46 @@
 import { writable } from "svelte/store";
 
-export const currentPage = writable("home");
+//- Page Store
+export const currentPage = writable("manage-tasks");
+
+//- Tasks Store
+const createTasks = () => {
+	const { subscribe, set, update } = writable([
+		{ id: 1, content: "water plants", isCheckedOff: false },
+		{ id: 2, content: "buy groceries", isCheckedOff: false },
+		{ id: 3, content: "workout", isCheckedOff: false },
+	]);
+	return {
+		subscribe,
+		add: (task) => update((tasks) => [task, ...tasks]),
+		update: (taskId, editedTaskContent) =>
+			update((tasks) => {
+				const targetTask = tasks.find((task) => task.id === taskId);
+				targetTask.content = editedTaskContent;
+				return tasks;
+			}),
+		remove: (taskId) =>
+			update((tasks) => tasks.filter((task) => task.id === taskId)),
+		checkOff: (taskId) =>
+			update((tasks) => {
+				const targetTask = tasks.find((task) => task.id === taskId);
+				targetTask.isCheckedOff = !targetTask.isCheckedOff;
+				return tasks;
+			}),
+		reset: () => set([]),
+	};
+};
+
+export const tasks = createTasks();
+
+//- task
+	//- id
+	//- content
+	//- isCheckedOff
+
+//- bleep
+	//- id
+	//- content
+	//- minutely/hourly
+	//- isActivated
+	//- clickToConfirm
