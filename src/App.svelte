46f42dd97f7deff00e8f224b19319213fbdb7675
Daeram Chung <daeram.chung@gmail.com>
2022-07-20 18:34:36 +0900

    Change component names

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,22 @@
 <script>
 	import { currentPage } from "./stores.js";
 	import Navbar from "./components/Navbar.svelte";
-	import Home from "./pages/Home.svelte";
-	import ManageTasks from "./pages/ManageTasks.svelte";
-	import ManageBleeps from "./pages/ManageBleeps.svelte";
-	import Settings from "./pages/Settings.svelte";
+	import HomePage from "./pages/HomePage.svelte";
+	import ManageTasksPage from "./pages/ManageTasksPage.svelte";
+	import ManageBleepsPage from "./pages/ManageBleepsPage.svelte";
+	import SettingsPage from "./pages/SettingsPage.svelte";
 </script>
 
 <Navbar />
 <main>
 	{#if $currentPage === "home"}
-		<Home />
+		<HomePage />
 	{:else if $currentPage === "manage-tasks"}
-		<ManageTasks />
+		<ManageTasksPage />
 	{:else if $currentPage === "manage-bleeps"}
-		<ManageBleeps />
+		<ManageBleepsPage />
 	{:else if $currentPage === "settings"}
-		<Settings />
+		<SettingsPage />
 	{/if}
 </main>
 
