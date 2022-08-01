85efdb4d0407746625ba5a0d7aad3c3b5e315577
Daeram Chung <daeram.chung@gmail.com>
2022-08-01 17:36:44 +0900

    Refactor to use Section component

--- src/pages/ManageTasksPage.svelte
+++ src/pages/ManageTasksPage.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { tasks } from "../stores.js";
+	import Section from "../components/Section.svelte";
 	import Task from "../components/Task.svelte";
 
 	let inputVal;
@@ -14,42 +15,42 @@
 </script>
 
 <div class="Page">
-	<section>
-		<h2>Manage Tasks</h2>
+	<Section contentType="main-section">
+		<h2 slot="title">Manage Tasks</h2>
 
-		<ul class="tasks-list">
+		<ul slot="content">
 			{#each $tasks as task}
-				<li class="tasks-list__item">
+				<li>
 					<Task {task} />
 				</li>
 			{/each}
 		</ul>
 
-		<form on:submit|preventDefault={handleSubmit}>
+		<form slot="form" on:submit|preventDefault={handleSubmit}>
 			<input type="text" bind:value={inputVal} />
 			<button type="submit">Add</button>
 		</form>
-	</section>
+	</Section>
 
-	<section>
-		<h2>Configurations</h2>
+	<Section contentType="config-section">
+		<h2 slot="title">Configurations</h2>
 
-		<ul class="configs-list">
-			<li class="configs-list__item">
+		<ul slot="content">
+			<li>
 				<input type="checkbox" />
 				Reset after time
-				<ul class="configs-list__item__options-list">
-					<li class="configs-list__item__options-list__item">
+				<ul>
+					<li>
 						Reset at <input type="time" />
 					</li>
-					<li class="configs-list__item__options-list__item">
+					<li>
 						<input type="checkbox" />
 						Keep unfinished tasks
 					</li>
 				</ul>
 			</li>
 		</ul>
-	</section>
+	</Section>
 </div>
 
 <style>
@@ -59,72 +60,5 @@
 		justify-content: space-between;
 		align-items: center;
 		gap: 10px;
-
-		/* border: 1px solid red; */
-	}
-
-	section {
-		height: 100%;
-		display: flex;
-		flex-direction: column;
-		position: relative;
-
-		/* border: 1px solid green; */
-	}
-
-	h2 {
-		margin-bottom: 20px;
-	}
-
-	section:first-of-type {
-		min-width: 350px;
-		flex-grow: 1;
-	}
-
-	.tasks-list {
-		flex-grow: 1;
-		overflow: auto;
-	}
-
-	.tasks-list__item {
-		height: 40px;
-		margin-bottom: 10px;
-		padding-inline: 10px;
-
-		/* border: 1px solid orange; */
-	}
-
-	form {
-		width: 100%;
-		height: 35px;
-		display: flex;
-		position: sticky;
-		/* bottom: 0; */
-	}
-
-	input {
-		height: inherit;
-		flex-grow: 1;
-		background-color: #f9f9f9;
-		font-size: 1rem;
-	}
-
-	button {
-		width: 12%;
-		max-width: 50px;
-		height: inherit;
-	}
-
-	section:last-of-type {
-		min-width: 300px;
-	}
-
-	/* Configurations */
-	.configs-list {
-		padding-inline: 10px;
-	}
-
-	.configs-list__item__options-list {
-		padding-left: 25px;
 	}
 </style>
