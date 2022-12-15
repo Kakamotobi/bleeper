5ebb6a953670997708c95a66bf114a1ac469ea36
Kakamotobi <daeram.chung@gmail.com>
2022-12-16 00:53:57 +0900

    Componentize task form

---
+++ src/components/tasks/TaskForm.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { tasks } from "../../stores/tasksStore";
+
+	let inputVal;
+
+	const handleSubmit = () => {
+		const newTaskContent = inputVal.trim();
+		if (newTaskContent !== "") {
+			tasks.add(newTaskContent);
+			inputVal = "";
+		}
+	};
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+	<input
+		type="text"
+		placeholder="New task..."
+		maxlength="100"
+		required
+		bind:value={inputVal}
+	/>
+	<button type="submit">Add</button>
+</form>
--- src/components/tasks/TasksSection.svelte
+++ src/components/tasks/TasksSection.svelte
@@ -1,36 +1,20 @@
 <script>
-	import { tasks } from "../../stores/tasksStore.js";
 	import Section from "../shared/Section.svelte";
 	import Tasks from "./Tasks.svelte";
 	import ActionButtons from "../shared/ActionButtons.svelte";
-
-	//- For new task
-	let inputVal;
-
-	const handleSubmit = () => {
-		const newTaskContent = inputVal.trim();
-		if (newTaskContent !== "") {
-			tasks.add(newTaskContent);
-			inputVal = "";
-		}
-	};
+	import TaskForm from "./TaskForm.svelte";
 </script>
 
 <Section contentType="main-section">
 	<h2 slot="title">Tasks</h2>
 
-	<ActionButtons slot="buttons" target="tasks" buttons={["check", "uncheck", "clear"]} />
+	<ActionButtons
+		slot="buttons"
+		target="tasks"
+		buttons={["check", "uncheck", "clear"]}
+	/>
 
 	<Tasks slot="content" />
 
-	<form slot="form" on:submit|preventDefault={handleSubmit}>
-		<input
-			type="text"
-			placeholder="New task..."
-			maxlength="100"
-			required
-			bind:value={inputVal}
-		/>
-		<button type="submit">Add</button>
-	</form>
+	<TaskForm slot="form" />
 </Section>
